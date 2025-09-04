0b94502cf3310c4dc4b78a09b2179478be223623
Lucas Alex <lucasalexti@gmail.com>
2025-09-03 22:15:08 -0300

    classes refatoradas conforme novas interfaces

--- src/main/java/br/com/mariojp/solid/isp/OfficeService.java
+++ src/main/java/br/com/mariojp/solid/isp/OfficeService.java
@@ -1,11 +1,13 @@
 package br.com.mariojp.solid.isp;
 
 public class OfficeService {
-    private final MultiFunctionDevice device;
-    public OfficeService(MultiFunctionDevice device){ this.device = device; }
-    public void printReport(String content){
+    private final SimplePrinter device;
+
+    public OfficeService(SimplePrinter device) {
+        this.device = device;
+    }
+
+    public void printReport(String content) {
         device.print(content);
-        // BUG: chama scan sem necessidade
-        device.scan("audit.txt");
     }
 }
